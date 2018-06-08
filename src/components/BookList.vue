251fa3686ba6e4b56453460546593674daf51f60
Randale Watson <rwatson.comsci@gmail.com>
2018-06-08 17:22:11 -0500

    Part1 first attempt

---
+++ src/components/BookList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+    </div>
+</template>
+<script>
+    export default function data(){
+        title: "All Books"
+    }
+</script>
+<style>
+    h1, h2{
+        font-weight: normal;
+    }
+</style>
+
