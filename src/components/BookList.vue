4b44a22cdb00dc6ffdad23989b987cd8797337d1
Randale Watson <rwatson.comsci@gmail.com>
2018-06-08 17:40:57 -0500

    module2 complete

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <ul>
+            <li v-for='book in books'>
+                {{book.title}}:{{book.author}}
+            </li>
+        </ul>
     </div>
 </template>
 <script>
@@ -8,14 +13,27 @@
         name: "BookList",
         data: function(){
             return {
-                title: "All Books"
+                title: "All Books",
+                books: [
+                    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                    {title: 'American Gods', author: 'Neil Gaiman'},
+                    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                ]
             }
         }
     }
 </script>
 <style>
-    h1, h2{
+    h1, h2 {
         font-weight: normal;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {        
+        display: block;
+        margin: 0 10px;
     }
 </style>
 
