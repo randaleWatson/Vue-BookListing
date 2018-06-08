78de868dbe802ffd1c38806c924b81aa01143930
Randale Watson <rwatson.comsci@gmail.com>
2018-06-08 18:12:42 -0500

    Creating and addding book form

---
+++ src/components/BookForm.vue
@@ -0,0 +1,28 @@
+<template>
+    <form @submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
+        <input type="text" name="title" v-model="bookTitle" placeholder="Book Title">
+        <input type="text" name="author" v-model="bookAuthor" placeholder="Book Author">
+        <button type="submit" name="button">Add Book</button>
+    </form>
+</template>
+<script>
+export default {
+    name: 'BookForm',
+    props: ['books'],
+    data: function(){
+        return {
+            bookTitle: '',
+            bookAuthor: ''
+        }
+    },
+    methods: {
+        bookSubmit: function(bookTitle, bookAuthor){
+            this.$emit('addBook', bookTitle, bookAuthor);
+        }
+    }
+};
+</script>
+<style>
+
+</style>
+
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,18 +2,21 @@
     <div>
         <h1>{{title}}</h1>
         <ul>
-            <book-item :book='book' v-for='book in books'>
+            <book-item :book="book" v-for="book in books">
                 {{book.title}}:{{book.author}}
             </book-item>
         </ul>
+        <book-form @addBook="appendBook"/>
     </div>
 </template>
 <script>
     import BookItem from './BookItem';
+    import BookForm from './BookForm';
     export default {
         name: "BookList",
         components: {
-            BookItem: BookItem
+            BookItem,
+            BookForm
         },
         data: function(){
             return {
@@ -23,6 +26,11 @@
                     {title: 'American Gods', author: 'Neil Gaiman'},
                     {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
                 ]
+            }
+        },
+        methods: {
+            appendBook: function(bookTitle, bookAuthor) {
+                this.books.push({title: bookTitle, author: bookAuthor});
             }
         }
     };
